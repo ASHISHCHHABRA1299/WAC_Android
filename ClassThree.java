95d624f7a5990ea15f43864311d5cf93157a5bb8
hp <chhabraashish123@gmail.com>
2021-05-08 14:58:32 +0530

    Class three added

---
+++ ClassThree.java
@@ -0,0 +1,253 @@
+package wacWorkshop1;
+
+//class bulb {
+//
+//	// Data Members
+//	boolean isOn;
+//
+//	// Data Functions or Methods
+//	public void turnOn() {
+//		isOn = true;
+//	}
+//
+//	public void turnOff() {
+//		isOn = false;
+//	}
+//
+//}
+
+//class calculator {
+//	int value1;
+//	int value2;
+//
+//	// default constructor
+//	
+//	calculator(){
+//		value1=5;
+//		value2=4;
+//	}
+//	
+//	//parametrised constructor
+//	calculator(int a,int b){
+//		value1=a;
+//		value2=b;
+//	}
+//	
+//	// sum
+//
+//	public void sum() {
+//		System.out.println(value1 + value2);
+//	}
+//
+//	// subtract
+//	public void subtract() {
+//		System.out.println(value1 - value2);
+//	}
+//
+//	// multiply
+//	public void multiply() {
+//		System.out.println(value1 * value2);
+//	}
+//
+//	// divide
+//	public void divide() {
+//		System.out.println(value1 / value2);
+//	}
+//
+//}
+
+//class student {
+//	String name;
+//	int rollno;
+//	static int number_of_students;
+//
+//	student() {
+//		name = null;
+//		rollno = 0;
+//
+//	}
+//
+//	student(String n, int r) {
+//		name = n;
+//		rollno = r;
+//		number_of_students++;
+//	}
+//
+//	public String getName() {
+//		return name;
+//	}
+//
+//	public int getRollNo() {
+//		return rollno;
+//	}
+//
+//	public static int getnoofstudents() {
+//		return number_of_students;
+//	}
+//}
+
+//class addition {
+
+// sum of 2 numbers
+//	public void sum(int a, int b) {
+//		System.out.println(a + b);
+//	}
+
+//	public void sum(float a, float b) {
+//		System.out.println(a + b);
+//	}
+//
+//	// sum of 3 numbers
+//	public void sum(int a, int b, int c) {
+//		System.out.println(a + b + c);
+//	}
+
+//	public void sum(int... ar) {
+//		int sum = 0;
+//		for (int a : ar)
+//			sum += a;
+//		System.out.println(sum);
+//	}
+//
+//}
+
+//class student {
+//	String name;
+//	int rollno;
+//
+//	student() {
+//		this("Ashish",1);
+//		System.out.println("Welcome Student");
+//	}
+//
+//	student(String name, int rollno) {
+////		this();// calls the default constructor
+//		this.name = name;
+//		this.rollno = rollno;
+//	}
+//
+//	public String getName() {
+//		System.out.println(this.getRollNo());
+//		return name;
+//	}
+//
+//	public int getRollNo() {
+//		return rollno;
+//	}
+//
+//}
+
+public class ClassThree {
+
+	public static void main(String[] args) {
+//		bulb b = new bulb();  // creation of first object
+//		bulb b1 = new bulb(); // creation of second object
+//		System.out.println(b.isOn);  // false
+//		System.out.println(b1.isOn); // false
+//		b.turnOn();
+//		System.out.println(b.isOn); //true
+//		System.out.println(b1.isOn); //false
+
+//		calculator c = new calculator();
+//		c.sum();
+//		c.subtract();
+//		c.multiply();
+//		c.divide();
+
+//		student s=new student("Niloy", 1);
+//		student s1=new student("Ayush", 2);
+//		student s2=new student("Ayushi", 3);
+//		System.out.println(s.getName());
+//		System.out.println(s.getRollNo());
+//		System.out.println(s1.getName());
+//		System.out.println(s1.getRollNo());
+//		System.out.println(s2.getName());
+//		System.out.println(s2.getRollNo());
+//		System.out.println(student.getnoofstudents());
+
+//		addition a = new addition();
+//		a.sum(2, 3);
+//		a.sum(1, 3, 2,2);
+//		student s = new student();
+//		System.out.println(s.getName());
+//		System.out.println(s.getRollNo());
+		
+		
+//		int nst=1; //total number of stars
+//		int n=5;  //total number of rows
+//		int row=1;  // counter 
+//		while(row<=n) {
+//			
+//			int cst=1;
+//			while(cst<=nst) {
+//				System.out.print("* ");
+//				cst++;
+//			}
+//			System.out.println();
+//			row++;
+//			nst++;
+//		}
+		
+		
+		
+//		int nst=5;
+//		int n=5;
+//		int row=1;
+//		int nsp=0;
+//		while(row<=n) {
+//			
+//			int csp=1;
+//			while(csp<=nsp) {
+//				System.out.print(" ");
+//				csp++;
+//			}
+//			
+//			int cst=1;
+//			while(cst<=nst) {
+//				System.out.print("*");
+//				cst++;
+//			}
+//			System.out.println();
+//			nsp++;
+//			nst--;
+//			row++;
+//		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
